6bd355e534618520c8f6328aef935eb3400656cc
Dustin Tierney <dustintierney@rockethomes.com>
2022-07-29 12:40:00 -0400

    added the writeToFile function

--- index.js
+++ index.js
@@ -1,12 +1,65 @@
 // TODO: Include packages needed for this application
 const markdown = require('./utils/generateMarkdown');
 const inquirer = require('inquirer');
+const fs = require('fs')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'Project Title:',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'Project Description:',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'Installation Instructions:',
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: 'Useage Info:',
+        name: 'usageInfo'
+    },
+    {
+        type: 'input',
+        message: 'Contribution Guidelines:',
+        name: 'contribution'
+    },
+    {
+        type: 'input',
+        message: 'Tests:',
+        name: 'tests'
+    },
+    {
+        type: 'input',
+        message: 'License Type:',
+        name: 'license',
+        choices: ['MIT', 'GNU', 'Apache', 'BSD', 'ISC', 'Other']
+    },
+    {
+        type: 'input',
+        message: 'GitHub Username:',
+        name: 'github'
+    },
+    {
+        type: 'input',
+        message: 'Email Address:',
+        name: 'email'
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if(err) throw new Error(err);
+        console.log('README Successfully Generated!');
+    }); 
+}
 
 // TODO: Create a function to initialize app
 function init() {}
