cd8eb918b6ba6b1a21309c65ab090ada8d330225
Dustin Tierney <dustintierney@rockethomes.com>
2022-08-01 15:00:41 -0400

    Added the methods to display the license info in the README

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,21 +1,71 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+    if(data.license != null) {
+        let color;
+        switch (data.license) {
+            case 'MIT':
+                color = 'Yellow'
+                break;
+            case 'GNU':
+                color = 'Blue'
+                break;
+            case 'Apache':
+                color = 'green'
+                break;
+            case 'BSD':
+                color = 'orange'
+                break;
+            case 'ISC':
+                color = 'blue'
+                break;
+            case 'Other':
+                color = 'lightgrey'
+                break;                
+        }
+        return `[![License](http://img.shields.io/badge/license-${data.license}-${color}.svg)`;
+    }
+    return '';
+}
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {}
+    if(data.license != null) {
+        return `https://choosealicense.com/licenses/mit/${data.license}/`;
+    }
+    return '';
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+    function renderLicenseLink(license) {}
+    if(data.license != null) {
+        fetch(`https://api.github.com/licenses/${data.license}`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            return data[0].body;
+        });
+    }
+    return '';
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return
-   `# ${data.title}
+  return;
+  `
+    renderLicenseBadge(data.license) 
+   # ${data.title}
 
-    ## Table of Contents
+   ## Description
+
+   ${data.description}
+
+   ## Table of Contents
+
     *[Description](#description)
     *[Installation](#installation)
     *[Usage](#usage)
@@ -24,25 +74,30 @@ function generateMarkdown(data) {
     *[License](#license)
     *[Questions]{#questions}
 
-    ## Description
-    ${data.description}
-
     ## Installation
+
     ${data.installation}
 
     ## Usage
+
     ${data.usageInfo}
 
+    ## License
+
+    renderLicenseBadge(${data.license})
+    renderLicenseLink(${data.license})
+    renderLicenseSection(${data.license})
+
     ## Contributing
+
     ${data.contribution}
 
     ## Tests
+
     ${data.tests}
 
-    ## License
-    ${data.license}
-
     ## Questions
+    
     GitHib Profile: htttps://github.com/${data.github}
     You can reach me at ${data.email} with any questions.
    `;
