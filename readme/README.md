68d8f73f70a5a9660a607dc7e83ad9b438928df4
Dustin Tierney <dustintierney@rockethomes.com>
2022-10-17 17:37:25 -0400

    submitting challenge

--- readme/README.md
+++ readme/README.md
@@ -1,34 +1,35 @@
 
-   # 
+        # README Challenge
+     
+        ## Description
+        Generating a README file for projects
+     
+        ## Table of Contents
+        * [Installation](#installation)
+        * [Usage](#usage)
+        * [Contributing](#contributing)
+        * [Tests](#tests)
+        * [License](#license)
+        * [Questions](#questions)
+     
+        ## Installation
+        Clone repo and install Inquirer
+     
+        ## Usage
+        Generate a README
+     
+        ## License
+        ![License](https://img.shields.io/badge/license-MIT-yellow.svg) <br />
+        https://choosealicense.com/licenses/MIT/ 
+        
 
-   ## Description
-   
-
-   ## Table of Contents
-   * [Installation](#installation)
-   * [Usage](#usage)
-   * [Contributing](#contributing)
-   * [Tests](#tests)
-   * [License](#license)
-   * [Questions](#questions)
-
-   ## Installation
-   
-
-   ## Usage
-   
-
-   ## License
-   ![License](https://img.shields.io/badge/license-BSD-orange.svg) <br />
-   https://choosealicense.com/licenses/BSD/
-
-   ## Contributing
-   
-
-   ## Tests
-   
-
-   ## Questions
-   GitHib Profile: htttps://github.com/ <br />
-   You can reach me at  with any questions.
-   
+        ## Contributing
+        Please do
+     
+        ## Tests
+        No tests
+     
+        ## Questions
+        GitHib Profile: htttps://github.com/dtierney21 <br />
+        You can reach me at dtierney@email.com with any questions.
+        
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,38 +1,38 @@
+const axios = require('axios');
 // Function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-    if(license != null) {
+    if (license != null) {
         let color;
         switch (license) {
             case 'MIT':
-                color = 'yellow'
+                color = 'yellow';
                 break;
             case 'GNU':
-                color = 'blue'
+                color = 'blue';
                 break;
             case 'Apache':
-                color = 'green'
+                color = 'green';
                 break;
             case 'BSD':
-                color = 'orange'
+                color = 'orange';
                 break;
             case 'ISC':
-                color = 'blue'
+                color = 'blue';
                 break;
             case 'Other':
-                color = 'lightgrey'
-                break;                
+                color = 'lightgrey';
+                break;
         }
         return `https://img.shields.io/badge/license-${license}-${color}.svg`;
     }
     return '';
 }
 
-
 // Function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-    if(license != null) {
+    if (license != null) {
         return `https://choosealicense.com/licenses/${license}/`;
     }
     return '';
@@ -41,13 +41,11 @@ function renderLicenseLink(license) {
 // Function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-    if(license != null) {
-        fetch(`https://api.github.com/licenses/${license}`)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            return data[0].body;
+    if (license != null) {
+        console.log('entering section method');
+        axios.get(`https://api.github.com/licenses/${license}`).then((resp) => {
+            //console.log(`leaving section method: ${resp.data.body}`);
+            return resp.data.body;
         });
     }
     return '';
@@ -55,40 +53,41 @@ function renderLicenseSection(license) {
 
 // Function to generate markdown for README
 function generateMarkdown(data) {
-  return `
-   # ${data.title}
+    return `
+        # ${data.title}
+     
+        ## Description
+        ${data.description}
+     
+        ## Table of Contents
+        * [Installation](#installation)
+        * [Usage](#usage)
+        * [Contributing](#contributing)
+        * [Tests](#tests)
+        * [License](#license)
+        * [Questions](#questions)
+     
+        ## Installation
+        ${data.installation}
+     
+        ## Usage
+        ${data.usageInfo}
+     
+        ## License
+        ![License](${renderLicenseBadge(data.license)}) <br />
+        ${renderLicenseLink(data.license)} 
+        ${renderLicenseSection(data.license)}
 
-   ## Description
-   ${data.description}
-
-   ## Table of Contents
-   * [Installation](#installation)
-   * [Usage](#usage)
-   * [Contributing](#contributing)
-   * [Tests](#tests)
-   * [License](#license)
-   * [Questions](#questions)
-
-   ## Installation
-   ${data.installation}
-
-   ## Usage
-   ${data.usageInfo}
-
-   ## License
-   ![License](${renderLicenseBadge(data.license)}) <br />
-   ${renderLicenseLink(data.license)}
-
-   ## Contributing
-   ${data.contribution}
-
-   ## Tests
-   ${data.tests}
-
-   ## Questions
-   GitHib Profile: htttps://github.com/${data.github} <br />
-   You can reach me at ${data.email} with any questions.
-   `;
+        ## Contributing
+        ${data.contribution}
+     
+        ## Tests
+        ${data.tests}
+     
+        ## Questions
+        GitHib Profile: htttps://github.com/${data.github} <br />
+        You can reach me at ${data.email} with any questions.
+        `;
 }
 
 module.exports = generateMarkdown;
